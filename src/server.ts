bcb0224ac5f7a31cb977585f1f9b52e13d7c059c
mairon.vilela <maironvilela@hotmail.com>
2024-06-27 12:22:14 -0300

    init project

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", async (_req, _res) => {
+    return "Hello, Worlds!"
+});
+
+    app.listen({
+    port: 3333
+}).then(() =>{
+    console.log("Server is running on port 3333");
+})
+
