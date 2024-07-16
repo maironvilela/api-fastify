6f4de918e0635d95eef4324678a4752f86d2d4ce
Mairon Vilela <maironvilela@hotmail.com>
2024-07-16 12:22:55 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import { app } from './config/app'
 
 app
   .listen({
-    port: 3333,
-    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    host: '0.0.0.0',
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')
