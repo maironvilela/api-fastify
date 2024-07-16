a4e69cd693669945491a133b16730f03f22cc60d
mairon.vilela <maironvilela@hotmail.com>
2024-07-16 12:05:47 -0300

    add configuration server.ts file

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { app } from './config/app'
 app
   .listen({
     port: 3333,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('Server is running on port 3333')
